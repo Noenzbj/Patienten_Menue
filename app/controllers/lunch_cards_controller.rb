96596a9d76031ebcddb51c7025f007242e19f73b
Noenzbj <arturabramow@gmx.de>
2020-09-27 06:40:37 +0200

    Show Seite für Vollkost Mittag hinzugefügt

--- app/controllers/lunch_cards_controller.rb
+++ app/controllers/lunch_cards_controller.rb
@@ -2,4 +2,8 @@ class LunchCardsController < ApplicationController
   def index
     @lunch_cards = LunchCard.all
   end
+
+  def show
+    @lunch_card = LunchCard.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'index', to: 'lunch_cards#index'
+  resources :lunch_cards
   
 end
